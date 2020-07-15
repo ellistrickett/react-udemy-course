c39ea8370e2df01734772a47c1c09a65614fdab2
Ellis Trickett <ellis.trickett@gmail.com>
2020-07-15 10:59:42 +0100

    viewing request results with axios from unsplash api

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID l6yjg8Rn3l8uDyB6KYpgRZXMcwNtI6dRxsBp_0Zp_c4'
+      }
+    });
   }
 
   render () {
