bb3981dc7d4395830b2409fb302db6de0c2104df
Ellis Trickett <ellis.trickett@gmail.com>
2020-07-16 19:01:12 +0100

    rendering a list of videos

--- videos/src/component/App.js
+++ videos/src/component/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from './VideoList';
 
 const KEY = 'AIzaSyCh00tVCTXZB177-xXoJzQ9YmubOBSGmmE';
 
@@ -25,7 +26,7 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        I have {this.state.videos.length} videos
+        <VideoList videos={this.state.videos}/>
       </div>
     )
   }
