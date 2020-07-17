8836d644a217acac11f020a5d36cff3afd6445f8
Ellis Trickett <ellis.trickett@gmail.com>
2020-07-17 09:45:37 +0100

    displaying a video player

--- videos/src/component/VideoDetail.js
+++ videos/src/component/VideoDetail.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import VideoList from './VideoList';
 
 const VideoDetail = ({ video }) => {
   if (!video) {
     return <div>Loading...</div>;
   }
+
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
+
   return (
-  <div className="ui segment">
-    <h4 className="ui header">{video.snippet.title}</h4>
-  <p className="ui segment">{video.snippet.description}</p>
-  </div>
+   <div>
+     <div className="ui embed">
+       <iframe src={videoSrc} />
+     </div>
+     <div className="ui segment">
+       <h4 className="ui header">{video.snippet.title}</h4>
+       <p className="ui segment">{video.snippet.description}</p>
+     </div>  
+   </div> 
   );
 };
 
