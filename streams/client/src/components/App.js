b788c98c89a54fa72eb43f32a7741c6f0968b93e
Ellis Trickett <ellis.trickett@gmail.com>
2020-08-08 17:16:37 +0100

    scaffold components

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+  <div>
+    <BrowserRouter>
+    </BrowserRouter>
+  </div>
+  )
 };
 
 export default App;
