b96fd41bbff04d25b55ea9d084238681f703bdf5
Ellis Trickett <ellis.trickett@gmail.com>
2020-08-10 12:25:56 +0100

    displaying validation messages

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-  renderInput({ input, label }) {
+  renderInput({ input, label, meta }) {
     return (
       <div className="field">
         <label>{label}</label>
         <input {...input}/>
+        <div>{meta.error}</div>
       </div>
     )
   }
@@ -18,14 +19,29 @@ class StreamCreate extends React.Component {
   render() {
     return (
       <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
-        <Field name="Title" component={this.renderInput} label="Enter Title" />
-        <Field name="Description" component={this.renderInput} label="Enter Description" />
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field name="description" component={this.renderInput} label="Enter Description" />
         <button className="ui button primary">Submit</button>
       </form>
     )
   }
 };
 
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = 'You must enter a title';
+  }
+
+  if (!formValues.description) {
+    errors.description = 'You must enter a description';
+  }
+
+  return errors;
+}
+
 export default reduxForm({
-  form: 'streamCreate'
+  form: 'streamCreate',
+  validate
 })(StreamCreate);
